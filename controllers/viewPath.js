c27b3ed041a40ca237fe358641da3caa2d58a96d
AMGOcyber123 <aayumodi123@gmail.com>
2021-11-19 13:55:11 +0530

    fixed view path

--- controllers/viewPath.js
+++ controllers/viewPath.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose') 
 
 const displayPaths = async(req,res) =>{
-    const user =`User:  /user/details\ \n
-                    /user/add\ \n
-                    /usesr/details\ \n\n
-            Member: /member/add\ \n
-                    /member/display/:gid\ \n
-                    /member/display\ \n\n
-            Group:  /group/create\ \n
-                    /group/display\ \n
-                    /group/display/:id\ \n
-                    /group/display/user/:id\ \n
-                    /group/valid\ \n
-                    /group/user/:gid\ \n\n
-            Meeting:/meet/create\ \n
-                    /meet/getall/:gid\ \n
-                    /meet/get/:mid\ \n 
-                    /meet/update/:mid\ \n
-                    /meet/delete/:mid\ \n
+    const user =`User:  /user/details\ <br>
+                    /user/add\ <br>
+                    /usesr/details\ <br><br> `
+    const member= `Member: /member/add\ <br>
+                    /member/display/:gid\ <br>
+                    /member/display\ <br><br>`
+    const group = `Group:  /group/create\ <br>
+                    /group/display\ <br>
+                    /group/display/:id\ <br>
+                    /group/display/user/:id\ <br>
+                    /group/valid\ <br>
+                    /group/user/:gid\ <br><br>`
+    const meeting=`Meeting:/meet/create\ <br>
+                    /meet/getall/:gid\ <br>
+                    /meet/get/:mid\ <br> 
+                    /meet/update/:mid\ <br>
+                    /meet/delete/:mid\ <br>
                     `
-    res.send(user)
+    res.send(user+member+group+meeting)
 }
 
 module.exports = {displayPaths}
